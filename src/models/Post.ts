b07b26dbc432ae85c9558c001693ce3c83341011
Cami Codina <camy.codi@gmail.com>
2024-07-12 17:17:41 +0000

    Cambios visuales en el Muro

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -5,14 +5,14 @@ export class Post {
     id: number;
     texto: string;
     etiqueta: Etiqueta;
-    autor: Usuario;
+    usuario: Usuario;
     likes: Usuario[]
     
-    constructor(id: number, texto: string, etiqueta: Etiqueta, autor: Usuario, likes: Usuario[]) {
+    constructor(id: number, texto: string, etiqueta: Etiqueta, usuario: Usuario, likes: Usuario[]) {
         this.id = id;
         this.texto = texto;
         this.etiqueta = etiqueta;
-        this.autor = autor;
+        this.usuario = usuario;
         this.likes = likes;
     }
 }
